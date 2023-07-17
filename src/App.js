31ba4a1674371021c3e424e83446149dfd1e2497
SaulBassoJr <saulbassojr02@gmail.com>
2023-07-17 16:55:41 -0300

    Create Project

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import{BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Container from './components/layout/Container';
+import Home from './components/home/Home';
+
+import Navbarr from './components/layout/Navbarr';
+import Footer from './components/layout/Footer'
+
+import DisplayClientes from './components/clientes/DisplayClientes';
+import ManterClientes from './components/clientes/ManterClientes';
+import ManterServicos from './components/servicos/ManterServicos';
+import ManterVeiculos from './components/veiculos/ManterVeiculos';
+import ManterUsuarios from './components/usuarios/ManterUsuarios';
+import ManterOs from './components/os/ManterOs';
+
+function App() {
+  return (
+    <Router>
+      <Navbarr/>
+      <Container className="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/clientes' element={<DisplayClientes/>}/>
+          <Route exact path='/clientes/novocliente' element={<ManterClientes/>}/>
+          <Route exact path='/os/novaOs' element={<ManterOs/>}/>
+          <Route exact path='/servicos/novoservico' element={<ManterServicos/>}/>
+          <Route exact path='/veiculos/novoveiculo' element={<ManterVeiculos/>}/>
+          <Route exact path='/usuarios/novousuario' element={<ManterUsuarios/>}/>
+          {/* <Route exact path='/usuarios' element={<UsuariosListagem/>}/>
+          <Route exact path='/usuarios/novousuario' element={<UsuariosCadastro/>}/>
+          <Route exact path='/os' element={<OsListagem/>}/>
+          <Route exact path='/os/novaos' element={<OsCadastro/>}/>
+          <Route exact path='/veiculos' element={<VeiculosListagem/>}/>
+          <Route exact path='/veiculos/novoveiculo' element={<VeiculosCadastro/>}/> */}
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
