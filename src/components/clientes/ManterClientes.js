373b64a50c34b2d0c6416de1b7bb08c035a3e4dd
SaulBassoJr <saulbassojr02@gmail.com>
2023-09-16 11:44:49 -0300

    Added the post function in the ManterClientes.js component

--- src/components/clientes/ManterClientes.js
+++ src/components/clientes/ManterClientes.js
@@ -1,28 +1,103 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import '../layout/sectionLayout.css';
-import {IoStopCircleSharp, IoSave} from 'react-icons/io5';
+import { IoStopCircleSharp, IoSave } from 'react-icons/io5';
 
-function ManterClientes(){
-    return (       
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function ManterClientes() {
+
+    const [formData, setFormData] = useState({
+        nome: '',
+        cpf: '',
+        rg: '',
+        cidade: '',
+        endereco: '',
+        cep: ''
+    });
+    const [error, setError] = useState(null);
+    const [response, setResponse] = useState([]);
+
+
+    // const handleInputChange = (e) => {
+    //     const { name, value } = e.target;
+    //     setFormData({ ...formData, [name]: value });
+    // };
+    const handleInputChange = (e, fieldName) => {
+        const { value } = e.target;
+        setFormData(prevFormData => ({
+            ...prevFormData,
+            [fieldName]: value
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Enviar os dados para a API usando axios
+        try {
+            const response = await axios.post('https://localhost:7029/SCGPD/Cliente', formData, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = response.data;
+        } catch (error) {
+            console.error('Erro ao enviar os dados:', error);
+            if (error.response) {
+                // O servidor respondeu com um código de status fora do intervalo de 2xx
+                console.log('Data da resposta:', error.response.data);
+                console.log('Status da resposta:', error.response.status);
+                console.log('Headers da resposta:', error.response.headers);
+            } else if (error.request) {
+                // A requisição foi feita, mas não houve resposta
+                console.log('Requisição feita, mas não houve resposta:', error.request);
+            } else {
+                // Algo aconteceu durante a configuração da requisição que causou o erro
+                console.log('Erro ao configurar a requisição:', error.message);
+            }
+        }
+    };
+
+
+
+
+
+    return (
         <section className='main_section -bgheight'>
             <h1>Cadastrar Cliente</h1>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formNome">
                     <Form.Label>*Nome</Form.Label>
-                    <Form.Control type="name" placeholder="Nome" />
+                    <Form.Control
+                        type="name"
+                        placeholder="Nome"
+                        value={formData.nome}
+                        onChange={(e) => handleInputChange(e, 'nome')}
+                    />
                 </Form.Group>
 
 
-                <Form.Group className="variantpar" controlId="formBasicEmail">
+                <Form.Group className="variantpar" controlId="formCPF">
                     <div className="inputpar">
                         <div className='space'>
                             <Form.Label>*CPF / CNPJ</Form.Label>
-                            <Form.Control type="text" placeholder="Numero do CPF ou CNPJ" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Numero do CPF ou CNPJ"
+                                value={formData.cpf}
+                                onChange={(e) => handleInputChange(e, 'cpf')}
+                            />
                         </div>
                         <div>
                             <Form.Label>RG</Form.Label>
-                            <Form.Control type="text" placeholder="Numero do RG" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Numero do RG"
+                                value={formData.rg}
+                                onChange={(e) => handleInputChange(e, 'rg')}
+                            />
                         </div>
                     </div>
                 </Form.Group>
@@ -49,25 +124,35 @@ function ManterClientes(){
                 <Form.Group className="variantpar" controlId="formBasicEmail">
                     <div className="inputpar">
                         <div className='space'>
-                        <Form.Label>*CEP</Form.Label>
-                        <Form.Control type="text" placeholder="CEP"/>
+                            <Form.Label>*CEP</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="CEP"
+                                value={formData.cep}
+                                onChange={(e) => handleInputChange(e, 'cep')}
+                            />
                         </div>
 
                         <div className='space'>
-                        <Form.Label>*Cidade</Form.Label>
-                        <Form.Control type="text" placeholder="Cidade"/>
+                            <Form.Label>*Cidade</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Cidade"
+                                value={formData.cidade}
+                                onChange={(e) => handleInputChange(e, 'cidade')}
+                            />
                         </div>
 
                         <div >
-                        <Form.Label>*UF</Form.Label>
-                        <Form.Select type='checkbox'>
-                            <option>Selecione</option>
-                            <option>M</option>
-                            <option>F</option>
-                            <option>Outro</option>
-                        </Form.Select>
+                            <Form.Label>*UF</Form.Label>
+                            <Form.Select type='checkbox'>
+                                <option>Selecione</option>
+                                <option>M</option>
+                                <option>F</option>
+                                <option>Outro</option>
+                            </Form.Select>
                         </div>
-                    </div>       
+                    </div>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -78,7 +163,12 @@ function ManterClientes(){
                         </div>
                         <div className='space'>
                             <Form.Label>*Logradouro</Form.Label>
-                            <Form.Control type="text" placeholder="Logradouro" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Logradouro"
+                                value={formData.endereco}
+                                onChange={(e) => handleInputChange(e, 'endereco')}
+                            />
                         </div>
                         <div>
                             <Form.Label>*Numero</Form.Label>
@@ -86,13 +176,13 @@ function ManterClientes(){
                         </div>
                     </div>
                 </Form.Group>
-                
-                <Button variant="secondary" type="submit">
-                   <IoSave/>Salvar
+
+                <Button variant="secondary" type="submit" >
+                    <IoSave />Salvar
                 </Button>
 
                 <Button variant="secondary" type="submit">
-                   <IoStopCircleSharp/> Cancelar
+                    <IoStopCircleSharp /> Cancelar
                 </Button>
             </Form>
         </section>
