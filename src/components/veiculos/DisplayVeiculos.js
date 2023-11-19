67cd08f5702327809e632a3b88bbd961dc0d5f76
SaulBassoJr <saulbassojr02@gmail.com>
2023-11-19 12:37:57 -0300

    Paginate the Veiculos table

--- src/components/veiculos/DisplayVeiculos.js
+++ src/components/veiculos/DisplayVeiculos.js
@@ -1,13 +1,16 @@
+import Button from 'react-bootstrap/Button';
 import VeiculoDelete from './VeiculoDelete'
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Alert from 'react-bootstrap/Alert';
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 import '../layout/button-styles.css';
 import '../layout/buscador-styles.css'
 import '../layout/sectionLayout.css';
+import '../layout/table.css';
 
 function DisplayVeiculos() {
     const [error, setError] = useState(null);
@@ -19,6 +22,9 @@ function DisplayVeiculos() {
     const [q, setQ] = useState("");
     const [searchParam] = useState(["placa", "renavam"]);
     const [filterParam, setFilterParam] = useState(["All"]);
+
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(5);
 
     const getVeiculos = async () => {
         try {
@@ -36,6 +42,35 @@ function DisplayVeiculos() {
     }, []);
 
     const data = Object.values(veiculos);
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = search(data).slice(indexOfFirstItem, indexOfLastItem);
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(search(data).length / itemsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    const renderTableRows = () => {
+        return currentItems.map((veiculo) => (
+            <tr key={veiculo.id}>
+                <td>{veiculo.marca}</td>
+                <td>{veiculo.modelo}</td>
+                <td className="text-end">{veiculo.placa}</td>
+                <td className="text-end">{veiculo.renavam}</td>
+                <td className="text-center">{boolToSimNao(veiculo.debitos)}</td>
+                <td className="text-center">{boolToSimNao(veiculo.financiamento)}</td>
+                <td className="text-center">
+                    <VeiculoDelete id={veiculo.id} handleRemove={removeVeiculo} />
+                </td>
+            </tr>
+        ));
+    };
+
+    const handlePagination = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
     function search(veiculos) {
         return veiculos.filter((veiculo) => {
@@ -71,6 +106,58 @@ function DisplayVeiculos() {
     };
 
 
+    // Lógica para renderizar os botões de paginação
+    const renderPaginationButtons = () => {
+        const totalItems = search(data).length;
+        const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+        const pages = [];
+        const delta = 1; // Quantidade de botões antes e depois da página atual
+
+        let start = Math.max(1, currentPage - delta);
+        let end = Math.min(totalPages, currentPage + delta);
+
+        if (totalPages > 3) {
+            if (currentPage <= delta + 1) {
+                end = 2 * delta + 1;
+            } else if (currentPage >= totalPages - delta) {
+                start = totalPages - 2 * delta;
+            }
+        }
+
+        for (let i = start; i <= end; i++) {
+            pages.push(
+                <li className="list" key={i}>
+                    <Button className={`buttonList ${currentPage === i ? 'activePage' : ''}`} onClick={() => handlePagination(i)}>
+                        {i}
+                    </Button>
+                </li>
+            );
+        }
+
+        return (
+            <ul className="pagination-list">
+                {currentPage > 1 && (
+                    <li className="list">
+                        <Button className="buttonList" onClick={() => handlePagination(currentPage - 1)}>
+                            <FaAngleLeft />
+                        </Button>
+                    </li>
+                )}
+                {pages}
+                {currentPage < totalPages && (
+                    <li className="list">
+                        <Button className="buttonList" onClick={() => handlePagination(currentPage + 1)}>
+                            <FaAngleRight />
+                        </Button>
+                    </li>
+                )}
+            </ul>
+        );
+    };
+
+
+
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -101,60 +188,26 @@ function DisplayVeiculos() {
                 )}
 
                 <Table responsive bordered size="sm" >
-                    <thead >
+                    <thead className="table">
                         <tr>
-                            <th>Marca</th>
-                            <th>Modelo</th>
-                            <th>Placa</th>
-                            <th>Renavam</th>
-                            <th>Debitos</th>
-                            <th>Financiamento</th>
+                            <th className="text-center">Marca</th>
+                            <th className="text-center">Modelo</th>
+                            <th className="text-center">Placa</th>
+                            <th className="text-center">Renavam</th>
+                            <th className="text-center">Debitos</th>
+                            <th className="text-center">Financiamento</th>
                             <th> </th>
                         </tr>
                     </thead>
                     <tbody >
-                        {search(data)?.map((veiculo) =>
-                            <tr key={veiculo.id} >
-                                <td>
-                                    {veiculo.marca}
-
-                                </td>
-                                <td>
-
-                                    {veiculo.modelo}
-
-                                </td>
-                                <td>
-
-                                    {veiculo.placa}
-
-                                </td>
-                                <td>
-                                    {veiculo.renavam}
-
-                                </td>
-                                <td>
-
-                                {boolToSimNao(veiculo.debitos)}
-
-                                </td>
-                                <td>
-
-                                {boolToSimNao(veiculo.financiamento)}
-
-                                </td>
-                                <td>
-                                    <VeiculoDelete
-                                        id={veiculo.id}
-                                        handleRemove={removeVeiculo}
-                                    />
-
-                                </td>
-                            </tr>
-                        )}
+                        {renderTableRows()}
                     </tbody>
 
                 </Table>
+
+                <div className="paginat">
+                    {renderPaginationButtons()}
+                </div>
 
             </section>
         )
